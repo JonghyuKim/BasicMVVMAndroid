6166f7d539cbccc49f79df9b93b396ba3a1ed3ef
jonghyu <jong0827@gmail.com>
2019-10-17 18:01:03 +0900

    fix transition after scroll

--- presentation/src/main/java/com/hyu/basic/mvvm/android/presentation/preview/PreviewFragment.kt
+++ presentation/src/main/java/com/hyu/basic/mvvm/android/presentation/preview/PreviewFragment.kt
@@ -9,9 +9,12 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.hyu.basic.mvvm.android.presentation.SelectViewModel
 import com.hyu.basicmvvmandroid.presentation.R
 import com.hyu.basicmvvmandroid.presentation.databinding.FragmentPreviewBinding
+
+
 
 class PreviewFragment : Fragment(){
 
@@ -24,21 +27,44 @@ class PreviewFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return DataBindingUtil.inflate<FragmentPreviewBinding>(inflater, R.layout.fragment_preview, container, false).apply {
-            recyclerView.apply {
-
-                adapter = PreviewListAdapter(selectViewModel)
-                model = viewModel
-                lifecycleOwner = this@PreviewFragment.viewLifecycleOwner
-
-                viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener{
-                    override fun onPreDraw(): Boolean {
-                        viewTreeObserver.removeOnPreDrawListener(this)
-                        startPostponedEnterTransition()
-                        return true
-                    }
-                })
-            }
+            recyclerViewSetting(recyclerView)
+            model = viewModel
+            lifecycleOwner = this@PreviewFragment.viewLifecycleOwner
         }.root
+    }
+
+    private fun recyclerViewSetting(recyclerView: RecyclerView){
+        recyclerView.apply {
+
+            adapter = PreviewListAdapter(selectViewModel)
+
+            viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener{
+                override fun onPreDraw(): Boolean {
+                    viewTreeObserver.removeOnPreDrawListener(this)
+                    startPostponedEnterTransition()
+                    return true
+                }
+            })
+
+            addOnLayoutChangeListener(object : View.OnLayoutChangeListener{
+                override fun onLayoutChange(v: View?,left: Int,top: Int,right: Int,bottom: Int,oldLeft: Int,oldTop: Int,oldRight: Int,oldBottom: Int) {
+
+                    removeOnLayoutChangeListener(this)
+
+                    val position = selectViewModel.selectIndex.value!!
+
+                    layoutManager?.let{layoutManager ->
+                        val viewAtPosition = layoutManager.findViewByPosition(position)
+
+                        if (viewAtPosition == null || layoutManager.isViewPartiallyVisible(viewAtPosition,false,true)) {
+                            post { layoutManager.scrollToPosition(position) }
+                        }
+                    }
+
+
+                }
+            })
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
