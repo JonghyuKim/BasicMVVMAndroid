56c99d035c41c8cb0bf0dc9d8beed4b9fbac47b5
jonghyu <jong0827@gmail.com>
2019-10-17 11:15:21 +0900

    fix fragment transition

--- presentation/src/main/java/com/hyu/basic/mvvm/android/presentation/detail/DetailFragment.kt
+++ presentation/src/main/java/com/hyu/basic/mvvm/android/presentation/detail/DetailFragment.kt
@@ -9,12 +9,15 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.hyu.basic.mvvm.android.presentation.SelectViewModel
+import com.hyu.basic.mvvm.android.presentation.util.imageloader.IImageLoader
 import com.hyu.basicmvvmandroid.presentation.R
 import com.hyu.basicmvvmandroid.presentation.databinding.FragmentDetailBinding
+import org.koin.android.ext.android.inject
 
 class DetailFragment : Fragment(){
 
     private val selectViewModel : SelectViewModel by activityViewModels()
+    private val imageLoader by inject<IImageLoader>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,8 +29,21 @@ class DetailFragment : Fragment(){
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 ivDetailMainImage.transitionName = "previewItem${selectViewModel.selectIndex}"
             }
+            with(imageLoader) {
+
+                onCompliteBinding = {
+                    startPostponedEnterTransition()
+                }
+
+                bindImg(requireContext(), ivDetailMainImage, selectViewModel.selectModel.value!!.image)
+            }
             selectModel = selectViewModel
             lifecycleOwner = this@DetailFragment.viewLifecycleOwner
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        postponeEnterTransition()
     }
 }
